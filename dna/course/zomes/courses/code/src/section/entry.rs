ddb8c1185fa59c0b6dfe236a0fdf57d0948807c4
falyhery <32633685+falyhery@users.noreply.github.com>
2020-08-06 13:47:28 +0200

    Add entry.rs to Section

---
+++ dna/course/zomes/courses/code/src/section/entry.rs
@@ -0,0 +1,57 @@
+use hdk::{
+    entry_definition::ValidatingEntryType, 
+    holochain_core_types::{dna::entry_types::Sharing, validation::EntryValidationData},
+    holochain_json_api::{error::JsonError, json::JsonString},
+    holochain_persistence_api::cas::content::Address, 
+};
+use holochain_entry_utils::HolochainEntry; 
+
+#[derive(Serialize, Deserialize, Debug, DefaultJson, Clone)]
+pub struct Section {
+    pub title: String, 
+    pub timestamp: u64, 
+    pub anchor_address: Address, 
+}
+
+impl Section {
+    pub fn new(
+        title: String, 
+        timestamp: u64, 
+        anchor_address: Address, 
+    ) -> Self {
+        title: title, 
+        timestamp: timestamp, 
+        anchor_address: anchor_address,
+    }
+}
+
+impl HolochainEntry for Section  {
+    fn entry_type() -> String {
+        String::from("course")
+    }
+}
+
+// Holochain entry definition for Section 
+pub fn section_entry_def() -> ValidatingEntryType {
+    entry!(
+        name: Section::entry_type(),
+        sharing: Sharing::Public, 
+        validation_package: || {
+            hdk::ValidationPackageDefinition::Entry
+        },
+        validation: | validation_data: hdk::EntryValidationData<Section>| {
+            match validation_data {
+                EntryValidationData::Create { .. } => {
+                    Ok(())
+                },
+                EntryValidationData::Modify { .. } => {
+                    Ok(())
+                }, 
+                EntryValidationData::Delete { .. } => {
+                    Ok(())
+                }
+            }
+        }, 
+        links: []
+    )
+}
